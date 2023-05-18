5d396c56275664f1ca854f31a5e686a64388cea5
Роман Шатиков <shatikovre@MacBook-Pro-Roman.local>
2023-05-18 20:47:52 +0300

    nthg

--- client/src/components/Pages/LoginPage.tsx
+++ client/src/components/Pages/LoginPage.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export default function LoginPage() {
-  return (
-    <div>
-      Страница входа на сайт
-    </div>
-  )
+export default function LoginPage(): JSX.Element {
+  return <div>Страница входа на сайт</div>;
 }
