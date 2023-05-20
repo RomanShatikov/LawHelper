1e5d89c2dd8e8b814b90072348513e25c6c96af7
Роман Шатиков <shatikovre@MacBook-Pro-Roman.local>
2023-05-20 22:31:48 +0300

    50percent

--- client/src/components/Pages/AnswerPage.tsx
+++ client/src/components/Pages/AnswerPage.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'reactstrap';
+import Pagination from '@mui/material/Pagination';
+import { QuestionType } from '../../types/questions/questionType';
+import OneAnswer from '../UI/OneAnswer';
 
-export default function AnswerPage() {
+export default function AnswerPage(): JSX.Element {
+  // const questions = useAppSelector<QuestionType[]>((state) => state.question.questions);
+
   return (
-    <div>
-     Страница с вопросами
-    </div>
-  )
+      <OneAnswer />
+  );
 }
