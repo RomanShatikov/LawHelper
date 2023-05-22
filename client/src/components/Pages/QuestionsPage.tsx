4f67f4dc77c75b49f35b382f407d587ef330f025
Роман Шатиков <shatikovre@MacBook-Pro-Roman.local>
2023-05-22 15:29:18 +0300

    fix2

--- client/src/components/Pages/QuestionsPage.tsx
+++ client/src/components/Pages/QuestionsPage.tsx
@@ -1,56 +1,58 @@
 import React, { useEffect } from 'react';
-import SearchInputQuest from '../UI/SearchInputQuest';
-import Button from '@mui/material/Button';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Pagination from '@mui/material/Pagination';
-import GradeIcon from '@mui/icons-material/Grade';
+import SearchInputQuest from '../UI/SearchInputQuest';
 import { useAppDispatch, useAppSelector } from '../../features/hooks';
 import {
   getFirstQuestions,
   getQuestionsByPage,
 } from '../../features/redux/slices/questions/questionsThunk';
-import { QuestionType } from '../../types/questions/questionType';
-import { Stack } from '@mui/material';
-import axios from 'axios';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import type { QuestionType } from '../../types/questions/questionType';
 import MediaCard from '../UI/MediaCard';
+
+type PageCountFromBackend = {
+  pageCount: number;
+};
 
 export default function QuestionsPage(): JSX.Element {
   const questions = useAppSelector<QuestionType[]>((state) => state.question.questions);
-  const [pageCount, setPageCount] = React.useState(0);
+  const [pageCount, setPageCount] = React.useState<number>(0);
   const dispatch = useAppDispatch();
   const { id, title } = useParams();
 
   useEffect(() => {
     axios
-      .post('/questionsPageCount', { id, title })
+      .post<PageCountFromBackend>('/questionsPageCount', { id, title })
       .then((res) => setPageCount(res.data.pageCount))
       .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
     axios
-      .post('/questionsPageCount', { id, title })
+      .post<PageCountFromBackend>('/questionsPageCount', { id, title })
       .then((res) => setPageCount(res.data.pageCount))
       .catch((err) => console.log(err));
   }, [title]);
 
   useEffect(() => {
-    dispatch(getFirstQuestions(Number(id), title));
+    dispatch(getFirstQuestions({ id: Number(id), title }));
   }, []);
 
   useEffect(() => {
-    dispatch(getFirstQuestions(Number(id), title));
+    dispatch(getFirstQuestions({ id: Number(id), title }));
   }, [title]);
 
-  const paginationHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const page = Number(e.target.textContent);
-    dispatch(getQuestionsByPage(Number(id), page, title));
+  const paginationHandler = (e: React.MouseEvent<HTMLElement, MouseEvent>): void => {
+    const target = e.target as HTMLElement;
+    const page = Number(target.textContent);
+    dispatch(getQuestionsByPage({ id: Number(id), page, title }));
   };
 
   return (
     <>
       <SearchInputQuest />
-      {pageCount ? <Pagination count={pageCount} onClick={(e) => paginationHandler(e)} /> : null}
+      {pageCount ? <Pagination count={pageCount} onClick={paginationHandler} /> : null}
       {questions.length ? (
         questions?.map((question) => <MediaCard key={question?.id} question={question} />)
       ) : (
