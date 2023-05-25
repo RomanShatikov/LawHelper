7523bf69ff058559ac53ebae0144d75329384d1d
Роман Шатиков <shatikovre@MacBook-Pro-Roman.local>
2023-05-25 17:31:10 +0300

    cssworks

--- client/src/components/UI/LoginForm.tsx
+++ client/src/components/UI/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, FormGroup } from 'react-bootstrap';
 import { Input, Label } from 'reactstrap';
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -13,93 +13,27 @@ export default function LogForm(): JSX.Element {
   const dispatch = useAppDispatch();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.currentTarget))as LoginForm
+    const data = Object.fromEntries(new FormData(e.currentTarget)) as LoginForm;
     dispatch(loginUserThunk(data));
   };
 
-
   return (
     <Form onSubmit={handleSubmit}>
-
       <FormGroup>
         <Label for="exampleEmail">Email</Label>
-        <Input id="exampleEmail" name="email"  type="email" />
+        <Input id="exampleEmail" name="email" type="email" />
+        {erors.EmailEror && <p>{erors.EmailEror}</p>}
       </FormGroup>
 
       <FormGroup>
         <Label for="examplePassword">Password</Label>
-        <Input
-          id="examplePassword"
-          name="password"
-         
-          type="password"
-        />
+        <Input id="examplePassword" name="password" type={isVisible ? 'text' : 'password'} />
+        <button type="button" onClick={() => setVisible(!isVisible)}>
+          <VisibilityIcon />
+        </button>
+        {erors.loginPasswordEror && <p>{erors.loginPasswordEror}</p>}
       </FormGroup>
-      <Button type='submit'>Войти</Button>
+      <Button type="submit">Войти</Button>
     </Form>
   );
 }
-
-//   return (
-//     <Container component="main" maxWidth="xs">
-//       <CssBaseline />
-//       <Box
-//         sx={{
-//           marginTop: 8,
-//           display: 'flex',
-//           flexDirection: 'column',
-//           alignItems: 'center',
-//         }}
-//       >
-//         <Avatar sx={{ m: 1, bgcolor: 'black' }}>
-//           <LockOutlinedIcon />
-//         </Avatar>
-//         <Typography component="h1" variant="h5">
-//           Login
-//         </Typography>
-//         <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-//           <Grid container spacing={2}>
-//             <Grid item xs={12} sm={6}>
-//               <TextField
-//                 autoComplete="given-name"
-//                 name="email"
-//                 required
-//                 fullWidth
-//                 id="email"
-//                 label="Почта"
-//                 value={input.email || ''}
-//                 onChange={inputHandler}
-//                 autoFocus
-//               />
-//               {error.firstName && <p>{error.firstName}</p>}
-//             </Grid>
-//             <Grid item xs={12} sm={6}>
-//               <TextField
-//                 required
-//                 fullWidth
-//                 id="password"
-//                 label="Пароль"
-//                 name="password"
-//                 value={input.password || ''}
-//                 onChange={inputHandler}
-//                 autoComplete="family-name"
-//               />
-//               {error.password && <p>{error.password}</p>}
-//             </Grid>
-//           </Grid>
-
-//           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-//             Войти
-//           </Button>
-//           <Grid container justifyContent="flex-end">
-//             <Grid item>
-//               <Link component={RouterLink} to="/login" variant="body2">
-//                 Уже есть аккаунт? Войдите
-//               </Link>
-//             </Grid>
-//           </Grid>
-//         </Box>
-//       </Box>
-//     </Container>
-//   );
-// }
